c28b061d1951174c6d490e54eda5ddbda9f92c2b
ShivamDh <shdh8888@yahoo.ca>
2017-02-26 00:54:17 -0500

    Modularized code for New Workout layout

--- app/src/main/java/shivamdh/com/fitness60/NewWorkout.java
+++ app/src/main/java/shivamdh/com/fitness60/NewWorkout.java
@@ -1,16 +1,23 @@
 package shivamdh.com.fitness60;
 
 import android.app.TimePickerDialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
+import android.widget.TableLayout;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Locale;
 
@@ -38,7 +45,57 @@ public class NewWorkout extends Fragment {
         TextView toStartWorkout = (TextView) theView.findViewById(R.id.time_start);
         toStartWorkout.setText(finalTime);
 
+
+
+
+//        ViewGroup myView = (ViewGroup) theView.findViewById(R.id.layout);
+        //.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+
+//        View n;
+//
+//        n = inflater.inflate(R.layout.activity_tables, container, false);
+//        myView.addView(n);
+
+
+        Activities one = new Activities(container, theView, inflater, getContext());
+        Activities two = new Activities(container, theView, inflater, getContext());
+
+//        final Button addSet = (Button) theView.findViewById(R.id.add_more_sets);
+//        addSet.setOnClickListener(new View.OnClickListener() {
+//
+//            @Override
+//            public void onClick(View v) {
+//
+//            }
+//        });
+
         return theView;
     }
+
+}
+
+class Activities {
+//    public static View myView;
+
+    Activities(ViewGroup mainContainer, View theLayout, LayoutInflater theInflate, Context theContext) {
+        LinearLayout myView = (LinearLayout) theLayout.findViewById(R.id.layout);
+        View n = theInflate.inflate(R.layout.activity_tables, mainContainer, false);
+        myView.addView(n);
+
+
+        LinearLayout newView = new LinearLayout(theContext);
+        newView.setLayoutParams(new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT));
+
+        myView.addView(newView);
+
+        ViewGroup.MarginLayoutParams margins = (ViewGroup.MarginLayoutParams) newView.getLayoutParams();
+        int top = 100;
+        margins.setMargins(0, 100, 0, 0);
+
+    }
+
+
 
 }
