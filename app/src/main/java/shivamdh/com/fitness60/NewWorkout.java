8c95642e1435cf0f15cdb2e20cdafec6e59aad5d
ShivamDh <shdh8888@yahoo.ca>
2017-03-02 00:38:18 -0500

    New workout portion of the app updated

--- app/src/main/java/shivamdh/com/fitness60/NewWorkout.java
+++ app/src/main/java/shivamdh/com/fitness60/NewWorkout.java
@@ -4,7 +4,10 @@ import android.app.TimePickerDialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,8 +15,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
-import android.widget.ScrollView;
 import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
@@ -26,6 +29,7 @@ import java.util.Locale;
  */
 public class NewWorkout extends Fragment {
 
+    private int activities = 0;
 
     public NewWorkout() {
         // Required empty public constructor
@@ -33,9 +37,9 @@ public class NewWorkout extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(final LayoutInflater inflater, final ViewGroup container,
                              Bundle savedInstanceState) {
-        View theView = inflater.inflate(R.layout.new_workout, container, false);
+        final View theView = inflater.inflate(R.layout.new_workout, container, false);
 
         SimpleDateFormat time = new SimpleDateFormat("HH:mm:ss", Locale.CANADA);
         Calendar theTime = Calendar.getInstance();
@@ -56,32 +60,53 @@ public class NewWorkout extends Fragment {
 //        n = inflater.inflate(R.layout.activity_tables, container, false);
 //        myView.addView(n);
 
+        final ArrayList<Activities> workoutActivities = new ArrayList<Activities>();
+        workoutActivities.add(new Activities(container, theView, inflater, getContext(), ++activities));
+        workoutActivities.add(new Activities(container, theView, inflater, getContext(), ++activities));
 
-        Activities one = new Activities(container, theView, inflater, getContext());
-        Activities two = new Activities(container, theView, inflater, getContext());
-
-//        final Button addSet = (Button) theView.findViewById(R.id.add_more_sets);
-//        addSet.setOnClickListener(new View.OnClickListener() {
-//
-//            @Override
-//            public void onClick(View v) {
-//
-//            }
-//        });
+        Button addActivities = (Button) theView.findViewById(R.id.new_activity);
+        addActivities.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                workoutActivities.add(new Activities(container, theView, inflater, getContext(), ++activities));
+            }
+        });
 
         return theView;
     }
 
+//    View.OnClickListener onClick = new View.OnClickListener() {
+//        @Override
+//        public void onClick(View v) {
+//            switch (v.getId()) {
+//                case R.id.delete_activity:
+//
+//                    break;
+//                default:
+//                    break;
+//            }
+//        }
+//    };
 }
 
-class Activities {
-//    public static View myView;
 
-    Activities(ViewGroup mainContainer, View theLayout, LayoutInflater theInflate, Context theContext) {
+class Activities implements View.OnClickListener{
+    private Button addSets, removeSets;
+//    public Button deleteActivity;
+    private TableRow newRow;
+    private Context theContext;
+    private EditText sets, reps, weight;
+    private TextView time;
+    private String finalTime;
+    private int setNumber, firstWeight, firstReps;
+    private TableLayout table;
+
+    Activities(ViewGroup mainContainer, View theLayout, LayoutInflater theInflate, Context aContext, int activityNum) {
+        theContext = aContext;
+
         LinearLayout myView = (LinearLayout) theLayout.findViewById(R.id.layout);
         View n = theInflate.inflate(R.layout.activity_tables, mainContainer, false);
-        myView.addView(n);
-
+        myView.addView(n, activityNum);
 
         LinearLayout newView = new LinearLayout(theContext);
         newView.setLayoutParams(new ViewGroup.LayoutParams(
@@ -94,8 +119,128 @@ class Activities {
         int top = 100;
         margins.setMargins(0, 100, 0, 0);
 
+        addSets = (Button) n.findViewById(R.id.add_more_sets);
+        addSets.setOnClickListener(this);
+        removeSets = (Button) n.findViewById(R.id.remove_sets);
+        removeSets.setOnClickListener(this);
+//        deleteActivity= (Button) n.findViewById(R.id.delete_activity);
+//        onClick(n);
+
+        table = (TableLayout) n.findViewById(R.id.activity1);
+        table.setColumnStretchable(0, true);
+        table.setColumnStretchable(1, true);
+        table.setColumnStretchable(2, true);
+        table.setColumnStretchable(3, true);
+        setNumber = 0;
+
+        //creating first row
+        createRow();
     }
 
+    private void createRow() {
+        newRow = new TableRow(theContext);
+        sets = new EditText(theContext);
+        weight = new EditText(theContext);
+        reps = new EditText(theContext);
+        time = new TextView(theContext);
+        setNumber++;
+        sets.setText(Integer.toString(setNumber));
+        if (setNumber == 1) {
+            createFirstRowOnly();
+        }
+        time.setText("00:00");
+        sets.setGravity(Gravity.CENTER);
+        weight.setGravity(Gravity.CENTER);
+        time.setGravity(Gravity.CENTER);
+        reps.setGravity(Gravity.CENTER);
+        newRow.addView(sets);
+        newRow.addView(weight);
+        newRow.addView(reps);
+        newRow.addView(time);
+        table.addView(newRow,setNumber);
+    }
+
+    private void createFirstRowOnly() {
+        weight.setHint("Weight"); //only for 1st one
+        weight.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                try {
+                    firstWeight = Integer.parseInt(s.toString());
+                } catch (Exception e) {
+
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        reps.setHint("Reps"); //only for 1st one
+        reps.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                try {
+                    firstReps = Integer.parseInt(s.toString());
+                } catch (Exception e) {
+
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.add_more_sets:
+                createRow();
+                //extra applicable only to 2nd row onwards
+                if (firstWeight == 0) {
+                    weight.setHint("Weight");
+                } else {
+                    weight.setText(Integer.toString(firstWeight));
+                }
+                if (firstReps == 0) {
+                    reps.setHint("Reps");
+                } else {
+                    reps.setText(Integer.toString(firstReps));
+                }
+                break;
+            case R.id.remove_sets:
+                if (setNumber > 0) { //check to not remove the header row
+                    table.removeViewAt(setNumber);
+                    setNumber--;
+                }
+                break;
+            default:
+                break;
+        }
 
 
+    }
+
+//
+//    @Override
+//    public void run() {
+//        SimpleDateFormat time = new SimpleDateFormat("mm:ss", Locale.CANADA);
+//        Calendar theTime = Calendar.getInstance();
+//        finalTime = time.format(theTime.getTime());
+//    }
 }
+
