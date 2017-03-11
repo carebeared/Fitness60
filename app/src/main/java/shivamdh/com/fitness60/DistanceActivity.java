a3a22a63605ff9ad6bb572930fa752edc3a7708b
ShivamDh <shdh8888@yahoo.ca>
2017-03-11 03:35:45 -0500

    Creating a new distance activity, to be a subclass of basic activity

---
+++ app/src/main/java/shivamdh/com/fitness60/DistanceActivity.java
@@ -0,0 +1,16 @@
+package shivamdh.com.fitness60;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class DistanceActivity extends Activities {
+
+
+    DistanceActivity(ViewGroup mainContainer, View theLayout, LayoutInflater theInflate, Context aContext, int activityNum, Activity currActivity) {
+        super(mainContainer, theLayout, theInflate, aContext, activityNum, currActivity);
+    }
+}
