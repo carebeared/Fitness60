809ab4222b8510bc3117694240b60d4e00849011
ShivamDh <shdh8888@yahoo.ca>
2017-03-14 23:57:12 -0400

    New bodyweight exercise derived from distance exercise

---
+++ app/src/main/java/shivamdh/com/fitness60/BodyweightExercise.java
@@ -0,0 +1,17 @@
+package shivamdh.com.fitness60;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class BodyweightExercise extends DistanceActivity {
+
+    public BodyweightExercise(ViewGroup mainContainer, View theLayout, LayoutInflater theInflate, Context aContext, int activityNum, Activity currActivity) {
+        super(mainContainer, theLayout, theInflate, aContext, activityNum, currActivity);
+    }
+
+
+
+}
