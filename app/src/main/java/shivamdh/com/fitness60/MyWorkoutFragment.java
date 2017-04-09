669457f4c1e831acfead5c4df02d0ef075eac36f
ShivamDh <shdh8888@yahoo.ca>
2017-04-08 23:55:49 -0400

    Rest of the project added

--- app/src/main/java/shivamdh/com/fitness60/MyWorkoutFragment.java
+++ app/src/main/java/shivamdh/com/fitness60/MyWorkoutFragment.java
@@ -1,6 +1,7 @@
 package shivamdh.com.fitness60;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -9,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,10 +18,14 @@ import android.widget.Toast;
  * A simple {@link Fragment} subclass.
  */
 public class MyWorkoutFragment extends Fragment {
-
+    Context theContext;
 
     public MyWorkoutFragment() {
         // Required empty public constructor
+    }
+
+    public void setTheContext (Context givenContext) {
+        theContext = givenContext;
     }
 
     @Override
@@ -42,6 +48,27 @@ public class MyWorkoutFragment extends Fragment {
         final TextView myText = (TextView) myView.findViewById(R.id.workout_text1);
         myText.setText("List of activities underneath");
 
+        LinearLayout workoutTab = (LinearLayout) myView.findViewById(R.id.myworkout_tab);
+
+        if (NewWorkout.done && NewWorkout.Save) {
+            TextView aText = new TextView(getContext());
+            Cursor theData;
+            if (NewWorkout.myData != null) {
+                theData = NewWorkout.myData.getAllData();
+                StringBuffer buffer = new StringBuffer();
+                if (theData.getCount() > 0) {
+                    while (theData.moveToNext()) {
+                        buffer.append("\n" + "Set " + theData.getString(0) + "\n");
+                        buffer.append("Weight " + theData.getString(1) + "\n");
+                        buffer.append("Reps " + theData.getString(2) + "\n");
+                        buffer.append("Timer " + theData.getString(3) + "\n");
+                    }
+                }
+                aText.setText(buffer.toString());
+                workoutTab.addView(aText);
+            }
+        }
+
         Button new_workout;
         new_workout = (Button) myView.findViewById(R.id.button_new_workout);
 
@@ -49,6 +76,7 @@ public class MyWorkoutFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 NewWorkout new_workout = new NewWorkout();
+                new_workout.setTheContext(theContext);
                 FragmentTransaction transaction = getFragmentManager().beginTransaction();
 
                 transaction.replace(R.id.fragment_container, new_workout);
--- app/src/main/java/shivamdh/com/fitness60/NewWorkout.java
+++ app/src/main/java/shivamdh/com/fitness60/NewWorkout.java
@@ -3,6 +3,7 @@ package shivamdh.com.fitness60;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
@@ -13,11 +14,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -27,19 +28,27 @@ import java.util.Timer;
 
 import static shivamdh.com.fitness60.Options.timerC;
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class NewWorkout extends Fragment implements View.OnClickListener {
+    DatabaseHelper db;
 
     private ArrayList<Activities> workoutActivities = new ArrayList<>();
     private LayoutInflater currInflate;
     private ViewGroup currContainer;
     private View theView;
     LinearLayout workoutTab;
+    Context appContext;
+    static WorkoutActivityDatabase myData;
+    public static boolean done = false;
+    public static boolean Save = false;
 
     public NewWorkout() {
         // Required empty public constructor
+        done = true;
+    }
+
+    public void setTheContext (Context aContext) {
+        appContext = aContext;
+//        db = new DatabaseHelper(appContext, 0);
     }
 
     public static class MyDialogFragment extends DialogFragment implements DialogInterface.OnClickListener {
@@ -52,22 +61,66 @@ public class NewWorkout extends Fragment implements View.OnClickListener {
         @Override
         public Dialog onCreateDialog(Bundle savedInstanceState) {
             return new AlertDialog.Builder(getActivity())
-                    .setMessage("Workout closed. Would you like to save it??").setPositiveButton("Yes", this)
+                    .setMessage("Workout closed. Would you like to save it?").setPositiveButton("Yes", this)
                     .setNegativeButton("No", this).create();
         }
 
         @Override
         public void onClick(DialogInterface dialog, int which) {
                 switch (which) {
-                    case DialogInterface.BUTTON_POSITIVE :
+                    case DialogInterface.BUTTON_POSITIVE:
+                        Save = true;
                         break;
-                    case DialogInterface.BUTTON_NEGATIVE :
+                    case DialogInterface.BUTTON_NEGATIVE:
+                        Save = false;
                         break;
                     default:
                         break;
                 }
             }
         }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d("About to", "DESTORRYYYYY");
+
+        //THIS IS ONLY TAKING THE DATA FROM THE FIRST WORKOUT ACTIVITY, NEED IT TO DO ALL ACTIVITIES
+//        Cursor previousData;
+//        String buffer;
+//        if (myData != null) {
+//            previousData = myData.getAllData();
+//            previousData.moveToFirst();
+//            if (!previousData.isAfterLast()) {
+//                buffer = previousData.getString(0);
+//            } else {
+//                buffer = "";
+//            }
+//        } else {
+//            buffer = "";
+//        }
+//        int previousSets;
+//        try {
+//            previousSets = Integer.parseInt(buffer);
+//        } catch (NumberFormatException e) {
+//            previousSets = 0;
+//        }
+
+        if (workoutActivities.get(0).getClass() == DistanceActivity.class)  { //distance activity
+            myData = new WorkoutActivityDatabase(appContext, false, workoutActivities.get(0).table); //create normal database
+        } else { //normal activity
+            myData = new WorkoutActivityDatabase(appContext, true, workoutActivities.get(0).table); //create normal database
+        }
+        Log.d("Cal", "ed");
+        myData.addData();
+        Log.d("Cal", "DONE");
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+    }
 
     @Override
     public View onCreateView(final LayoutInflater inflater, final ViewGroup container,
@@ -84,7 +137,7 @@ public class NewWorkout extends Fragment implements View.OnClickListener {
         TextView toStartWorkout = (TextView) theView.findViewById(R.id.time_start);
         toStartWorkout.setText(finalTime);
 
-        workoutActivities.add(new Activities(container, theView, inflater, getContext(), workoutActivities.size()+1, getActivity()));
+        workoutActivities.add(new Activities(container, theView, inflater, getContext(), workoutActivities.size()+1, getActivity(), true));
 
         Button addActivities = (Button) theView.findViewById(R.id.new_activity);
         addActivities.setOnClickListener(this);
@@ -110,17 +163,23 @@ public class NewWorkout extends Fragment implements View.OnClickListener {
                 if (timerC && workoutActivities.size() > 0) {
                     workoutActivities.get(workoutActivities.size()-1).setTimer.cancel();
                 }
-                workoutActivities.add(new Activities(currContainer, theView, currInflate, getContext(), workoutActivities.size()+1, getActivity()));
+                workoutActivities.add(new Activities(currContainer, theView, currInflate, getContext(), workoutActivities.size()+1, getActivity(), true));
+                break;
+            case R.id.bodyweight_exercise:
+                if (timerC && workoutActivities.size() > 0) {
+                    workoutActivities.get(workoutActivities.size()-1).setTimer.cancel();
+                }
+                workoutActivities.add(new Activities(currContainer, theView, currInflate, getContext(), workoutActivities.size()+1, getActivity(), false));
                 break;
             case R.id.remove_last_activity:
                 if (workoutActivities.size() > 0) {
-                    if (timerC) {
+                    if (timerC && workoutActivities.size() > 1) {
                         Activities nowLastActivity = workoutActivities.get(workoutActivities.size() - 2); // about to become the last activity now
                         if (workoutActivities.size() > 1 && nowLastActivity.setNumber > 0) { //start previous activities timer now, only if there was a timer left
 
-                            TableLayout getTable =  nowLastActivity.getTable();
+                            TableLayout getTable =  nowLastActivity.table;
                             TableRow wantedRow = (TableRow) getTable.getChildAt(nowLastActivity.getSetNumber());
-                            TextView resumedTimer = (TextView) wantedRow.getChildAt(3); //get the desired timer textview
+                            TextView resumedTimer = (TextView) wantedRow.getChildAt(wantedRow.getChildCount()-1); //get the desired timer textview
 
                             int previousM = nowLastActivity.getPreviousMins(resumedTimer.getText()); //get the previous minutes on timer
                             int previousS = nowLastActivity.getPreviousSecs(resumedTimer.getText()); //get the previous seconds form the timer
@@ -140,12 +199,6 @@ public class NewWorkout extends Fragment implements View.OnClickListener {
                     workoutActivities.get(workoutActivities.size()-1).setTimer.cancel();
                 }
                 workoutActivities.add(new DistanceActivity(currContainer, theView, currInflate, getContext(), workoutActivities.size()+1, getActivity()));
-                break;
-            case R.id.bodyweight_exercise:
-                if (timerC && workoutActivities.size() > 0) {
-                    workoutActivities.get(workoutActivities.size()-1).setTimer.cancel();
-                }
-                workoutActivities.add(new BodyweightExercise(currContainer, theView, currInflate, getContext(), workoutActivities.size()+1, getActivity()));
                 break;
             default:
                 break;
